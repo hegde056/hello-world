e724a65d250976cfb65fa5422243d7d80065dd29
hegde056 <suduhegde01@gmail.com>
2018-07-07 21:14:17 +0530

    tic tac toe one time play

--- tictactoe.py
+++ tictactoe.py
@@ -4,21 +4,34 @@
 #------------------------------------------Initialisations---------------------------------------------#
 #board = ['0','1','2','3','4','5','6','7','8','9']
 board = [' ']*10
-p1_result = 'none'
-p2_result = 'none'
-  
+turn = 'Player1'
+game_on =True
 #---------------------------------------------Functions-----------------------------------------------#
-def printBoard():
+def printBoard(board):
     print ('\t\t{1} | {2} | {3}\n\t\t--|---|--\n\t\t{4} | {5} | {6}\n\t\t--|---|--\n\t\t{7} | {8} | {9}'.format(*board))
 
-def edit_board(self):
-    pass
+def place_marker(board, marker, position):
+    board[position] = marker
+    
 
-def check_result(marker_p1,marker_p2):
-    pass
+def check_win(board,mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+    
+def space_check(board, position):
+    return board[position] == ' '
 
-def write_board():
-    pass
+def check_fullboard(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+    return True 
 
 
 #-----------------------------------------Execution---------------------------------------------------#
@@ -32,18 +45,48 @@ else:
 
 
 print("Player1 marker : {0}\tPlayer2 marker : {1}".format(marker_p1,marker_p2))
-print("Player1 to go first")
+print('This is the board: \n')
+printBoard(board)
+print('\n')
+print("Player1 to go first\n")
 
-while(p1_result!='won' or p1_result!='won'or p1_result!='tie'or p1_result!='tie'):
-    print("Player1 GO!!")
-    index = int(input("Select your space [1-9]: "))
-    write_board
-    board[index]=marker_p1
-    printBoard()
-    check_result(marker_p1,marker_p2)
-    print("Player2 GO!!")
-    index = int(input("Select your space [1-9]: "))
-    write_board
-    board[index]=marker_p2
-    printBoard()
-    check_result(marker_p1,marker_p2)
+
+
+while game_on:
+
+    
+    if turn == 'Player1':
+        print("Player1 GO!!")
+        index = int(input("Select your space [1-9]: "))
+        place_marker(board, marker_p1,index)
+        printBoard(board)
+        
+        if check_win(board,marker_p1):
+            print("Congratulations Player1. YOU WON !!!")
+            game_on = False
+        else :
+            if check_fullboard(board):
+                print("Game is a draw!")
+                game_on = False
+            else :
+                turn = 'Player2'
+    else :
+        #player 2
+        print("Player2 GO!!")
+        index = int(input("Select your space [1-9]: "))
+        place_marker(board, marker_p2,index)
+        printBoard(board)
+        
+        if check_win(board,marker_p2):
+            print("Congratulations Player2. YOU WON !!!")
+            game_on = False
+        else :
+            if check_fullboard(board):
+                printBoard(board)
+                print("Game is a draw!")
+                game_on = False
+            else :
+                turn = 'Player1'
+                
+
+
